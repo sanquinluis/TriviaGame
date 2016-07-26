92086d90abb82345e0913529f1296e927c09f5de
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-07-26 14:52:19 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
+
 $('.hideit').hide();
+$('.done').hide();
+$('.theCount').hide();
 	$('.btn').click( function(){
 		$('.hideit').show();
 			$('.btn').hide();
-	});
+			$('.done').show();
+		var theCount = 60;
+		var startTimer = setInterval(time, 1000);
+		function time(){
+			$('.btn').click(startTimer);
+  			theCount --;
+  			if (theCount == 0);
+  			clearInterval(startTimer);
+  			
+	};
+		console.log(startTimer);
+		$('.timeLeft').html("Time remaining: " + theCount + " seconds");
 
+	});
+	
 
 
 });
