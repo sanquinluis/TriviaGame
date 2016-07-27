ae3a16457517d6df25d998f8e2effdaf05280ac0
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-07-27 15:37:25 -0400

    3er Update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,48 @@
 $(document).ready(function(){
-
-$('.hideit').hide();
-$('.done').hide();
-$('.theCount').hide();
-	$('.btn').click( function(){
-		$('.hideit').show();
-			$('.btn').hide();
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+var theCount = 60;
+	$('.hideit').hide();
+	$('.done').hide();
+	$('.hidethis').hide();
+		$('.start').on ("click", function(){
+			$('.hideit').show();
+			$('.start').hide();
 			$('.done').show();
-		var theCount = 60;
-		var startTimer = setInterval(time, 1000);
-		function time(){
-			$('.btn').click(startTimer);
-  			theCount --;
-  			if (theCount == 0);
-  			clearInterval(startTimer);
+		var startTimer = setInterval(decrement, 1000);
+		function decrement(){	
+  			theCount--;
+  			// console.log(theCount);
+  			$('.timeLeft').html("Time remaining: " + theCount + " seconds");
+  			if (theCount === 1){
+  				$('.hideit').hide();
+  				$('.hidethis').show();
+  				$('.done').hide();
+  				clearInterval(theCount);
+  			}
   			
-	};
-		console.log(startTimer);
-		$('.timeLeft').html("Time remaining: " + theCount + " seconds");
-
+  			(theCount);
+	};			
 	});
+		$('input').on ("click", function(){
+
+		function radioSe(){
+			if ($('#correctop').attr("checked", "checked"));
+			correct++;
+			if ($('#wrongop').attr("checked","checked"));
+			incorrect++;
+	};
+			radioSe(correct);
+			console.log(incorrect);
+			console.log(correct);
+});
+		$('.done').on ("click",function(){
+			$('.hideit').hide();
+			$('.hidethis').show();
+			$('.done').hide();
+		
+	})
 	
 
 
