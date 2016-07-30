e7433c8ba3af198366f58ae3c936c7e913df165a
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-07-30 16:38:23 -0400

    hello Everyone

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 var correct = 0;
 var incorrect = 0;
-var unanswered = 0;
+var unanswered = 8;
 var theCount = 60;
+var startTimer;
+
 	$('.hideit').hide();
 	$('.done').hide();
 	$('.hidethis').hide();
@@ -10,39 +12,84 @@ var theCount = 60;
 			$('.hideit').show();
 			$('.start').hide();
 			$('.done').show();
-		var startTimer = setInterval(decrement, 1000);
+		startTimer = setInterval(decrement, 100);
 		function decrement(){	
   			theCount--;
   			// console.log(theCount);
   			$('.timeLeft').html("Time remaining: " + theCount + " seconds");
-  			if (theCount === 1){
+  			if (theCount === 0){
   				$('.hideit').hide();
   				$('.hidethis').show();
   				$('.done').hide();
-  				clearInterval(theCount);
+  				clearInterval(startTimer);
+  				$('.correct').html("Correct answers: "+correct);
+				$('.incorrect').html("Incorrect answers: "+incorrect);
+				$('.unanswered').html("Unanswered: "+unanswered);
+				startTimer = setInterval (function(){
+				clearInterval(startTimer);
+					correct = 0;
+					incorrect = 0;
+					unanswered = 8;
+					theCount = 60;
+					$('.hideit').hide();
+					$('.done').hide();
+					$('.hidethis').hide();
+					$('.start').show();
+			},3000);
+
   			}
-  			
-  			(theCount);
 	};			
-	});
-		$('input[name=answer]:radio').on ("click", function(){
-			if ($('#correctop').prop("checked", true));
-			correct++;
-			if ($('#wrongop').prop("checked",true));
-			incorrect++;
-			
-});
 		
-			
-			console.log(incorrect);
-			console.log(correct);
+		$('.correctop').click(function() {
+   		if($('.correctop').is(':checked')) { 
+   			function theCorrect(){
+   			console.log("correct");
+   			correct++;
+   			unanswered--;
+   			// $("#correct").html("Correct answers: " + correct);
+   			console.log(correct); 
+   		}
+   		 theCorrect();
+   	};
+   		
+		});
+		$('.wrongop').click(function(){
+	   		 if($('.wrongop').is(':checked')) { 
+	   			console.log("incorrect");
+	   			incorrect++;
+	   			unanswered--;
+	   			console.log(incorrect); 
+	   			}
 });
 		$('.done').on ("click",function(){
 			$('.hideit').hide();
 			$('.hidethis').show();
 			$('.done').hide();
+			$('.correct').html("Correct answers: "+correct);
+			$('.incorrect').html("Incorrect answers: "+incorrect);
+			$('.unanswered').html("Unanswered: "+unanswered);
+			startTimer = setInterval (function(){
+				clearInterval(startTimer);
+					correct = 0;
+					incorrect = 0;
+					unanswered = 8;
+					theCount = 60;
+					$('.hideit').hide();
+					$('.done').hide();
+					$('.hidethis').hide();
+					$('.start').show();
+
+
+
+			},3000);
+
 		
-	});
+		});
+		
+	});		
+			
+});
+		
 	
 
 
